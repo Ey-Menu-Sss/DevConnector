ebd928028537ec231443a34dfa3882e7d0b27574
Muhammad Qodir <muhammadeymenus@gmail.com>
2025-11-16 01:43:42 +0500

    chat V1

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import "../node_modules/boxicons/css/boxicons.min.css"
 // axios.defaults.baseURL = "http://localhost:8000/";
 axios.defaults.baseURL = "https://devconnector-backend-yy5b.onrender.com/";
 axios.defaults.headers.common["Content-Type"] = "application/json";
+axios.defaults.withCredentials = true;
 let token = localStorage.getItem("token");
 if (token) axios.defaults.headers.common["x-auth-token"] = `${token}`;
 // App file ...
